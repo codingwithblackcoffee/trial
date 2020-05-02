19dfb4e84fa40e23ee4a82bf04259ebeb789836f
codingwithblackcoffee <64619611+codingwithblackcoffee@users.noreply.github.com>
2020-05-02 21:06:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,15 +16,14 @@ output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 font = cv2.FONT_HERSHEY_COMPLEX
 
 #Video Capture
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("demo.mp4")
 frame_count = 1
 while (True):
     boxes = []
     confidences= []
     class_ids = []
     ret, img = cap.read()
-    demoimg = cv2.imread("demo2.jpeg")
-    seat_taken = [[0,0,180],[0,0,180]]
+    """img = cv2.imread("2.jpg")"""
 
     #storing original dimentions of the image
     height, width, channels = img.shape
@@ -55,23 +54,11 @@ while (True):
     for i in range(number_objects):
         color_box = (0,255,0)
         x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
         for box_dim in permanent_boxes:
             x1,y1,w1,h1 = box_dim
             if((abs((x +(w/2)) - (x1 + (w1/2))) < w/2) and (abs((y +(h/2)) - (y1 + (h1/2))) < h/2)):
-                if(label == "person"):
-                    if(permanent_boxes.index(box_dim) == 1):
-                        if(permanent_boxes[0][0] > x1):
-                            seat_taken[1] = [0,180,0]
-                        else:
-                            seat_taken[0] = [0,180,0]
-                    if(permanent_boxes.index(box_dim) == 0):
-                        if(permanent_boxes[1][0] > x1):
-                            seat_taken[1] = [0,180,0]
-                        else:
-                            seat_taken[0] = [0,180,0]                        
-                
-        
+                color_box = (255,0,0)
+        label = str(classes[class_ids[i]])
         if(label != "chair"):
             cv2.rectangle(img, (x,y), (x+w,y+h),color_box, 2)
             cv2.putText(img, label, (x,y+30), font, 1, (0,0,0), 3)
@@ -84,24 +71,7 @@ while (True):
             x,y,w,h = box_dim
             cv2.rectangle(img, (x,y), (x+w,y+w), (0,255,0), 2)
 
-    x,y,w,h = 440, 365, 335, 615
-    x1,y1,w1,h1 = 860, 365, 335, 615
-    sub_img = demoimg[y:y+h, x:x +w]
-    sub_img2 = demoimg[y1:y1+h1, x1:x1 +w1]
-    white_rect = np.ones(sub_img.shape, dtype=np.uint8) 
-    white_rect1 = np.ones(sub_img.shape, dtype=np.uint8) 
-    for matrix in range(len(white_rect)):
-        for matrix2 in range(len(white_rect[matrix])):
-            white_rect[matrix][matrix2] = seat_taken[0]
-    for matrix in range(len(white_rect1)):
-        for matrix2 in range(len(white_rect1[matrix])):
-            white_rect1[matrix][matrix2] = seat_taken[1]        
-        
-    res = cv2.addWeighted(sub_img, 0.5, white_rect, 0.5, 1.0)
-    res2 = cv2.addWeighted(sub_img2, 0.5, white_rect1, 0.5, 1.0)
-    demoimg[y:y+h, x:x+w] = res
-    demoimg[y1:y1+h1, x1:x1+w1] = res2
-    cv2.imshow("image",demoimg)
+    cv2.imshow("Image",img)
     frame_count +=1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
